2005f77b45a1c876931aa4157e0466c3ea029133
Michael <managedkaos@users.noreply.github.com>
2023-08-03 10:42:01 -0700

    Add docs

---
+++ Common_Resources/index_with_docs.py
@@ -0,0 +1,86 @@
+def handler(event, context):
+    import json
+
+    with open("data.json", "r") as f:
+        data = json.load(f)
+
+    if event["rawPath"] == "/":
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(data)
+        }
+
+    # Get the id from the path
+    id = event["rawPath"][1:]
+
+    if event["rawPath"] == "/docs":
+        # Create an HTML page with documentation
+        docs_page = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>API Documentation</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    line-height: 1.6;
+                    max-width: 800px;
+                    margin: 0 auto;
+                }
+                h1, h2 {
+                    color: #333;
+                    border-bottom: 1px solid #ccc;
+                }
+                p {
+                    margin-bottom: 16px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>API Documentation</h1>
+            <h2>GET /</h2>
+            <p>Returns all data in JSON format.</p>
+
+            <h2>GET /{id}</h2>
+            <p>Returns a specific item by its ID in JSON format.</p>
+        </body>
+        </html>
+        """
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "text/html",
+            },
+            "body": docs_page
+        }
+
+    # Check the id against each item in the data
+    for item in data:
+        # Return the item if the id matches
+        if item["id"] == id:
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+                "body": json.dumps(item)
+            }
+
+    # Return a 404 if the id doesn't match any item
+    else:
+        return {
+            "statusCode": 404,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps({
+                "message": f"id {id} not found",
+                "event": event,
+                "id": id
+            })
+        }
+
